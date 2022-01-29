7fcef4eaaf941ef381104b76c7279dc5113070b2
Emanuele117 <emanuele.pica7@gmail.com>
2022-01-29 15:48:07 +0100

    aggiunto select a edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -31,6 +31,19 @@
     </div>
 
     <div class="mb-3">
+      <label for="category_id" class="form-label">Categories</label>
+      <select class="form-control @error('category_id') is_invalid @enderror" name="category_id" id="category_id">
+        <option value="" >Uncategorized</option>
+        
+        @foreach($categories as $category)
+
+        <option value="{{$category->id}}" {{ $category->id === $post->category->id ? 'selected' : ''}} >{{$category->name}}</option>
+
+        @endforeach
+      </select>
+    </div>
+
+    <div class="mb-3">
       <label for="body" class="form-label">Body</label>
       <textarea class="form-control @error('body') is_invalid @enderror" name="" id="" rows="5">{{$post->body}}</textarea>
     </div>
